6282c3bf42664628ac48799ad3533b90a96e8584
Paulo Alexandre <pauloalexandre820@gmail.com>
2022-06-01 20:18:01 -0300

    alteração na api

--- lib/controller/service/api_request.dart
+++ lib/controller/service/api_request.dart
@@ -1,19 +1,17 @@
-
-
 import 'package:project_getex/controller/service/posts_provider.dart';
 
-class PostRepository{
+class PostRepository {
   final PostsConnectProvider connectApi;
 
   PostRepository(this.connectApi);
 
   // ignore: non_constant_identifier_names
-  GetPosts(){
-    return connectApi.getPosts(null);
-  }
-  // ignore: non_constant_identifier_names
-  GetUser(){
-    return connectApi.getUser();
+  GetPosts() {
+    return connectApi.getPosts();
   }
 
+  // ignore: non_constant_identifier_names
+  GetUser() {
+    return connectApi.getUser();
+  }
 }
--- lib/controller/service/posts_provider.dart
+++ lib/controller/service/posts_provider.dart
@@ -5,30 +5,28 @@ import 'package:project_getex/models/user.model.dart';
 const baseUrl = 'https://jsonplaceholder.typicode.com/posts';
 const baseUrl2 = 'https://jsonplaceholder.typicode.com/users';
 
-class PostsConnectProvider extends GetConnect{
-  Future<List<Post>?> getPosts(bool? loading, [void update]) async{
-    List<Post> postList = <Post> [];
-    final response = await get(baseUrl,decoder: (body){
+class PostsConnectProvider extends GetConnect {
+  Future<List<Post>?> getPosts() async {
+    List<Post> postList = <Post>[];
+    final response = await get(baseUrl, decoder: (body) {
       postList = Post.fromJson(body) as List<Post>;
-      return postList;
+      return postList.toList();
     });
-    if(response.hasError){
+    if (response.hasError) {
       throw Exception('Erro ao buscar Posts');
     }
-    return postList;
+    return postList.toList();
   }
-  Future<List<UserModel>?> getUser() async{
-    List<UserModel> postList = <UserModel> [];
-    final response = await get(baseUrl2,decoder: (body){
+
+  Future<List<UserModel>?> getUser() async {
+    List<UserModel> postList = <UserModel>[];
+    final response = await get(baseUrl2, decoder: (body) {
       postList = Post.fromJson(body) as List<UserModel>;
       return postList;
     });
-    if(response.hasError){
+    if (response.hasError) {
       throw Exception('Erro ao buscar Posts');
     }
     return postList;
   }
 }
-
-
-
